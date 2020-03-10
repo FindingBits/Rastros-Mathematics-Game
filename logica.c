ddf9a3c1120094e7e78921ad771209506cf0de0f
João Guedes <36474979+FrancesinhaMan@users.noreply.github.com>
2020-03-10 21:19:17 +0000

    Add files via upload

--- logica.c
+++ logica.c
@@ -7,7 +7,8 @@
 
 int jogar(ESTADO *estado, COORDENADA c){
     printf("Foi tentada uma jogada as coordenadas: \nLinha:%d\nColuna:%d",c.linha,c.coluna);
-    if((c.linha>=0 && c.linha<8) && (c.coluna>=0 && c.coluna<8)){
+    if((c.linha>=0 && c.linha<8) && (c.coluna>=0 && c.coluna<8) && estado->tab[c.linha][c.coluna]!='#' && estado->tab[c.linha][c.coluna]!='.'){
+        estado->tab[c.linha][c.coluna]='#';
         estado->num_jogadas+=1;
     }else{
         printf("Jogada invalida!");
