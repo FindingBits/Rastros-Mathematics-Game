bf3d807e40844272b6c82d3b47eb25371cc9c4a9
joodanic <62031763+joodanic@users.noreply.github.com>
2020-03-10 20:55:05 +0000

    Add files via upload
    parte da interface

---
+++ interface.h
@@ -0,0 +1,30 @@
+//
+// Created by João CArvalho on 10/03/2020.
+//
+
+#ifndef JOGO_INTERFACE_H
+#define JOGO_INTERFACE_H
+
+#endif //JOGO_INTERFACE_H
+// Estruturas de dados (devem ser colocadas no módulo correto da camada dos dados)
+typedef enum {VAZIO, BRANCA, PRETA} CASA;
+typedef struct {
+    int coluna,linha;
+} COORDENADA;
+typedef struct {
+    COORDENADA jogador1;
+    COORDENADA jogador2;
+} JOGADA;
+typedef JOGADA JOGADAS[32];
+typedef struct {
+    CASA tab[8][8];
+    JOGADAS jogadas;
+    int num_jogadas;
+    int jogador_atual;
+} ESTADO;
+
+
+void mostrar_tabuleiro (ESTADO *e);
+void entrada();
+*inicializar_estado();
+int interpretador(ESTADO *e);
