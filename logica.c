5681b48608b9d1aa481debd207e7818132ec80da
João Guedes <36474979+FrancesinhaMan@users.noreply.github.com>
2020-03-10 21:45:28 +0000

    Add files via upload

--- logica.c
+++ logica.c
@@ -4,12 +4,16 @@
 
 #include "logica.h"
 #include <stdio.h>
+#include <string.h>
 
 int jogar(ESTADO *estado, COORDENADA c){
     printf("Foi tentada uma jogada as coordenadas: \nLinha:%d\nColuna:%d",c.linha,c.coluna);
-    if((c.linha>=0 && c.linha<8) && (c.coluna>=0 && c.coluna<8) && estado->tab[c.linha][c.coluna]!='#' && estado->tab[c.linha][c.coluna]!='.'){
-        estado->tab[c.linha][c.coluna]='#';
+    if((c.linha>=0 && c.linha<8) && (c.coluna>=0 && c.coluna<8) && estado->tab[c.linha][c.coluna]!=PRETA && estado->tab[c.linha][c.coluna]!='.'){
+        estado->tab[c.linha][c.coluna]=PRETA;
+        //strcpy(estado->jogadas[estado->num_jogadas],"PRETA");
         estado->num_jogadas+=1;
+        if(estado->jogador_atual==1) estado->jogador_atual=2;
+        else estado->jogador_atual=1;
     }else{
         printf("Jogada invalida!");
         return 0;
