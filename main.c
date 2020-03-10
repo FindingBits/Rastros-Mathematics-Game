d9b5ba54649e6ccd3d5c2ce0529ad37f5088318b
joodanic <62031763+joodanic@users.noreply.github.com>
2020-03-10 22:16:22 +0000

    Add files via upload
    parte principal

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdio.h>
+#include <string.h>
+#include "interface.h"
+int main() {
+    entrada();
+    pedir_entrada();
+    ESTADO *e = inicializar_estado();
+    interpretador(e);
+    fim();
+    return 0;
+}
