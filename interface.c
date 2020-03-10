e133e76651fe3d0046010686cc81390358e81f7d
joodanic <62031763+joodanic@users.noreply.github.com>
2020-03-10 21:01:19 +0000

    Add files via upload
    parte da interface

--- interface.c
+++ interface.c
@@ -8,33 +8,31 @@
 #define BUF_SIZE 1024
 
 
-void entrada(){
-    int x,y;
+void entrada() {
+    int x, y;
     printf("Bem vindo ao jogo rastos\n\n\n");
     printf("1-Jogar  2-Informacao\n");
     printf("opcao:");
-    scanf("%d",&x);
-    if(x==1){
-    printf("1-jogador numero um   2-jogador numero dois   3-sair\n");
-    printf("Escolha o seu numero de jogador:");
-    scanf("%d",&y);
-    if(y==1){
-    printf("escolheu o jogador 1\n");
-    printf("o outro jogador e o numero 2\n");
+    scanf("%d", &x);
+    if (x == 1) {
+        printf("1-jogador numero um   2-jogador numero dois   3-sair\n");
+        printf("Escolha o seu numero de jogador:");
+        scanf("%d", &y);
+        if (y == 1) {
+            printf("escolheu o jogador 1\n");
+            printf("o outro jogador e o numero 2\n");
 
+        } else if (y == 2) {
+            printf("escolheu o jogador 2\n");
+            printf("o outro jogador e o numero 1\n");
+        }
     }
-    else if(y==2){
-    printf("escolheu o jogador 2\n");
-    printf("o outro jogador e o numero 1\n");
+        else if (x == 2) {
+            printf("created by Joao Carvalho, Joao Guedes, Miguel Tavares in 2020.\n");
+            printf("Este jogo foi realiazado no ambito da disciplina de laboratorios de algoritmia.\n");
+
+        }
     }
-
-}
-else if(x==2){
-printf("created by Joao Carvalho, Joao Guedes, Miguel Tavares in 2020.\n");
-printf("Este jogo foi realiazado no ambito da disciplina de laboratorios de algoritmia.\n");
-
-}
-}
 
 
 // Função que deve ser completada e colocada na camada da lógica do programa
