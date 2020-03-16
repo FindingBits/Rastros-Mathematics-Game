4553cda7b296edd67af7c923e7083239d2d5433e
João Carvalho <62031763+joodanic@users.noreply.github.com>
2020-03-16 21:12:39 +0000

    Add files via upload

--- interface.h
+++ interface.h
@@ -2,15 +2,33 @@
 // Created by João Carvalho on 10/03/2020.
 //
 
+/**
+@file interface.h
+Definição do estado e das funções que o manipulam
+*/
 #include "dados.h"
 #ifndef JOGO_INTERFACE_H
 #define JOGO_INTERFACE_H
 
 #endif //JOGO_INTERFACE_H
 
-
+/**
+\brief Tipo de interface para mostrar o tabuleiro
+*/
 void mostrar_tabuleiro (ESTADO *e);
+/**
+\brief Tipo de interface para mostrar o menu
+*/
 int entrada();
+/**
+\brief Tipo de interface para mostrar e receber as coordenadas
+*/
 void pedir_entrada();
+/**
+\brief Tipo de interface que le as jogadas e mostra essas jogadas no tabuleiro
+*/
 int interpretador(ESTADO *e);
+/**
+\brief Tipo de interface para mostrar quem e o vencedor
+*/
 void fim(int x);
