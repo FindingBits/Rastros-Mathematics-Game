0b326f06a04ad1e5b131e64e687d4845efbc1141
João Guedes <36474979+FrancesinhaMan@users.noreply.github.com>
2020-03-16 21:14:26 +0000

    Add files via upload

--- logica.h
+++ logica.h
@@ -1,9 +1,14 @@
 //
 // Created by João Guedes on 10/03/2020.
 //
-
+/**
+@file dados.h
+Definição do estado e das funções que o manipulam
+*/
 #ifndef GUIAO5_LOGICA_H
 #define GUIAO5_LOGICA_H
-
+/**
+\brief Função para jogar no tabuleiro
+*/
 #endif //GUIAO5_LOGICA_H
 int jogar(ESTADO *estado, COORDENADA c);
