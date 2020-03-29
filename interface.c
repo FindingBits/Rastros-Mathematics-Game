6c9452cc5bc9cab72fe8ecdaf19c7fcc1c167451
João Carvalho <62031763+joodanic@users.noreply.github.com>
2020-03-29 18:36:55 +0100

    Add files via upload

--- interface.c
+++ interface.c
@@ -23,19 +23,19 @@ int interpretador(ESTADO *e) {
                 for(i=0;i<obter_numero_de_jogadas(e);i++) {
                     if (e->jogador_atual = 1) {
                         if (e->jogadas[i].jogador1.linha != 0 && e->jogadas[i].jogador1.linha != 0) {
-                            printf("##Player1 (%d,%d)\n", (e->jogadas[i].jogador1.linha+1),
-                                   (e->jogadas[i].jogador1.coluna));
+                            printf("##Player1 (%d,%d)\n", (e->jogadas[i].jogador1.coluna),
+                                   (e->jogadas[i].jogador1.linha+1));
                         } else if (e->jogadas[i].jogador2.linha != 0 && e->jogadas[i].jogador2.linha != 0) {
-                            printf("##Player2 (%d,%d)\n", (e->jogadas[i].jogador2.linha+1),
-                                   (e->jogadas[i].jogador2.coluna));
+                            printf("##Player2 (%d,%d)\n", (e->jogadas[i].jogador2.coluna),
+                                   (e->jogadas[i].jogador2.linha+1));
                         }
                     } else if (e->jogador_atual = 2) {
                         if (e->jogadas[i].jogador2.linha != 0 && e->jogadas[i].jogador2.linha != 0) {
-                            printf("##Player2 (%d,%d)\n", (e->jogadas[i].jogador2.linha+1),
-                                   (e->jogadas[i].jogador2.coluna));
+                            printf("##Player2 (%d,%d)\n", (e->jogadas[i].jogador2.coluna),
+                                   (e->jogadas[i].jogador2.linha+1));
                         } else if (e->jogadas[i].jogador1.linha != 0 && e->jogadas[i].jogador1.linha != 0) {
-                            printf("##Player1 (%d,%d)\n", (e->jogadas[i].jogador1.linha+1),
-                                   (e->jogadas[i].jogador1.coluna));
+                            printf("##Player1 (%d,%d)\n", (e->jogadas[i].jogador1.coluna),
+                                   (e->jogadas[i].jogador1.linha+1));
                         }
                     }
                 }
