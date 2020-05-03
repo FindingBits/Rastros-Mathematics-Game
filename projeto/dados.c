085e61480c50ce3c74cfad7aa3a2e8bd826c3a00
João Guedes <36474979+FrancesinhaMan@users.noreply.github.com>
2020-05-03 22:54:25 +0100

    update bugfix

--- projeto/dados.c
+++ projeto/dados.c
@@ -13,8 +13,8 @@ ESTADO *inicializar_estado(){
         inicial.coluna = 4;
         e->passouBordas=0;
         e->ultima_jogada = inicial;
-        for(int i=0;i<8;i++) {
-            for (int j=0;j<8;j++) {
+        for(int i=0;i<9;i++) {
+            for (int j=0;j<9;j++) {
                 e->tab[i][j] = 0;
             }
         }
